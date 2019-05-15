8b097916279d6ff907e0095ebcb9980b0f4853d1
nguyengocbang <bang270197@gmail.com>
2019-05-15 22:30:31 +0700

    tongduongcheotrongmang2chieu

---
+++ mang2chieutongduongcheo/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int arr[][] = new int[3][3];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (int) (Math.random() * 20);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i == j) {
+                    sum += arr[i][j];
+                }
+            }
+        }
+        System.out.print("\n"+"Tong duong cheo trong mang la: "+sum);
+    }
+}
